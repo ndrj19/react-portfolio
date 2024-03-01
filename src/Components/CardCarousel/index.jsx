ed3c7c32610bcf95dbedfd1aa84d56a2c078ab0e
Andrej Hagenmüller <andrej.hagenmueller@gmail.com>
2024-03-01 16:43:53 +0000

    make projects responsive

--- src/Components/CardCarousel/index.jsx
+++ src/Components/CardCarousel/index.jsx
@@ -4,9 +4,30 @@ import CardGroup from "../CardGroup";
 const CardCarousel = () => {
   return (
     <>
-      <div id="carouselExample" className="carousel slide">
+      <div
+        id="carousel-small-screens"
+        className="carousel slide d-flex d-md-none"
+      >
         <div className="carousel-inner">
           <div className="carousel-item active">
+            <CardGroup />
+          </div>
+          <div className="carousel-item">
+            <CardGroup />
+          </div>
+          <div className="carousel-item">
+            <CardGroup />
+          </div>
+          <div className="carousel-item">
+            <CardGroup />
+          </div>
+          <div className="carousel-item">
+            <CardGroup />
+          </div>
+          <div className="carousel-item">
+            <CardGroup />
+          </div>
+          <div className="carousel-item">
             <CardGroup />
           </div>
           <div className="carousel-item">
@@ -16,7 +37,7 @@ const CardCarousel = () => {
         <button
           className="carousel-control-prev"
           type="button"
-          data-bs-target="#carouselExample"
+          data-bs-target="#carousel-small-screens"
           data-bs-slide="prev"
         >
           <span
@@ -28,7 +49,87 @@ const CardCarousel = () => {
         <button
           className="carousel-control-next"
           type="button"
-          data-bs-target="#carouselExample"
+          data-bs-target="#carousel-small-screens"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+      <div
+        id="carousel-medium-screens"
+        className="carousel slide d-none d-md-flex d-xl-none"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <CardGroup />
+          </div>
+          <div className="carousel-item">
+            <CardGroup />
+          </div>
+          <div className="carousel-item">
+            <CardGroup />
+          </div>
+          <div className="carousel-item">
+            <CardGroup />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carousel-medium-screens"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carousel-medium-screens"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+      <div
+        id="carousel-large-screens"
+        className="carousel slide d-none d-xl-flex"
+      >
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <CardGroup />
+          </div>
+          <div className="carousel-item">
+            <CardGroup />
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carousel-large-screens"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carousel-large-screens"
           data-bs-slide="next"
         >
           <span
--- src/Components/CardGroup/index.jsx
+++ src/Components/CardGroup/index.jsx
@@ -4,7 +4,14 @@ import ProjectCard from "../ProjectCard";
 const CardGroup = () => {
   return (
     <>
-      <div className="card-group">
+      <div className="card-group d-flex d-md-none">
+        <ProjectCard />
+      </div>
+      <div className="card-group d-none d-md-flex d-xl-none">
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+      <div className="card-group d-none d-xl-flex">
         <ProjectCard />
         <ProjectCard />
         <ProjectCard />
